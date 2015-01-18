7299c20e53ea8920e8dbf75f689fb691b2b95df2
BraydenGirard <braydengirard@icloud.com>
2015-01-18 15:50:55 -0500

    Added random numebr generators

--- cloud/main.js
+++ cloud/main.js
@@ -6,5 +6,22 @@ Parse.Cloud.define("hello", function(request, response) {
 });
 
 Parse.Cloud.job("spawnChests", function(request, status) {
+
+  //All randoms between and including 1 - 100
+  var weaponRandom = Math.floor(Math.random() * 100) + 1;
+  var potionRandom = Math.floor(Math.random() * 100) + 1;
+  var itemRandom = Math.floor(Math.random() * 100) + 1;
+
+  //Loop for the number of chests total
+  for(i=0; i<100; i++) {
+
+    if(weaponRandom < 11) {
+      //10% chance weapon random
+    } else if(weaponRandom < 31) {
+      //20% chance weapon random
+    }
+  }
+
+
   response.success("Chests spawned!");
 });
